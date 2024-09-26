cc10e4eb94b7afd1d0fb0bd45a59dd1fd123b686
drjasonnichols <jrn8969@gmail.com>
2024-09-25 23:07:01 -0700

    scroll

--- static/js/chatwindow.js
+++ static/js/chatwindow.js
@@ -164,7 +164,10 @@ function resizeViewport(offset = 0) {
         document.documentElement.style.setProperty('--vh', `${vh-(offset*0.01)}px`);
         //alert("resizing to: " + vh);
     }
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({
+        top: 0,
+        behavior: 'auto'  // This makes the scroll immediate
+      });
 }
 
 function keyboardOut(){
